19439f5e44003cfbaeb37136a85821118d71cab2
Alvarofm-22 <alvarofm1507@gmail.com>
2026-03-10 18:26:29 -0500

    subiendo mi tarea semana 9 proyecto 2

---
+++ src/main/java/BiblioUni/ControllerView/LoginViewController.java
@@ -0,0 +1,14 @@
+package BiblioUni.ControllerView;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginViewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "Login/index";
+    }
+
+}
